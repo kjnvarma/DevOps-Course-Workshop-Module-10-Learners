91b8ffbc54444bd2a46e704c4b2b78295bbb6070
JP <kjnvarma@gmail.com>
2021-07-15 17:48:42 +0100

    Module 10 workshop changes

--- WeatherForecast/Startup.cs
+++ WeatherForecast/Startup.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Identity.Web;
 
 namespace webapi
 {
@@ -18,6 +21,19 @@ namespace webapi
 
         public void ConfigureServices(IServiceCollection services)
         {
+
+             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));
+             services.AddAuthorization(config =>
+             {
+                config.AddPolicy(
+                "ApplicationPolicy",
+                policy => policy.RequireClaim(ClaimConstants.Roles, "WeatherApplicationRole")
+                );
+            });
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -48,6 +64,14 @@ namespace webapi
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -57,7 +81,7 @@ namespace webapi
 
             app.UseHttpsRedirection();
 
-            app.UseRouting();
+            //app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
